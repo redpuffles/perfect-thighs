c7d7bd81571cfb5488e8a5ec410250b77d47c51e
Rex-Xue-Lin <39583370+Rex-Xue-Lin@users.noreply.github.com>
2018-05-31 00:19:37 +1000

    Add files via upload

--- design.py
+++ design.py
@@ -9,35 +9,25 @@ Created on Wed May 30 00:42:18 2018
 
 import numpy as np
 
- 
-
 def design(THRESHOLD_MAX_INTERNAL_FUEL, THRESHOLD_MAX_OSCILLATION_INTERNAL_FUEL,
-alpha_b_array, alpha_p_array, max_internal_biofuel, 
-oscillation_internal_biofuel, final_external_biofuel) :
-
-    
-    
+           alpha_b_array, alpha_p_array, max_internal_biofuel,
+           oscillation_internal_biofuel, final_external_biofuel):
     # finding poor
-    
-    index = (np.argmax(final_external_biofuel))
-    print(index)
-    poor_alpha_b = (alpha_b_array[index//len(alpha_p_array)])
-    poor_alpha_p = (alpha_p_array[index%len(alpha_p_array)])
-    
-    
+    index = np.argmax(final_external_biofuel)
+    poor_alpha_b = alpha_b_array[index // len(alpha_p_array)]
+    poor_alpha_p = alpha_p_array[index % len(alpha_p_array)]
+
     # condition
-    c = ((max_internal_biofuel < THRESHOLD_MAX_INTERNAL_FUEL) & (oscillation_internal_biofuel < THRESHOLD_MAX_OSCILLATION_INTERNAL_FUEL))
-    
-    # set all false values to 0 
+    c = ((max_internal_biofuel < THRESHOLD_MAX_INTERNAL_FUEL) &
+         (oscillation_internal_biofuel < THRESHOLD_MAX_OSCILLATION_INTERNAL_FUEL))
+
+    # set all false values to 0
     # change to negative infinity?
     (final_external_biofuel[~c]) = 0
 
-    
-    
     # finding best
-    index = (np.argmax(final_external_biofuel))
-    best_alpha_b = (alpha_b_array[index//len(alpha_p_array)])
-    best_alpha_p = (alpha_p_array[index%len(alpha_p_array)])
-    
- 
+    index = np.argmax(final_external_biofuel)
+    best_alpha_b = alpha_b_array[index // len(alpha_p_array)]
+    best_alpha_p = alpha_p_array[index % len(alpha_p_array)]
+
     return(best_alpha_b, best_alpha_p, poor_alpha_b, poor_alpha_p)
