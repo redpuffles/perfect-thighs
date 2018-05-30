c93c934bdb98c13033e550bb444a8363deaa3770
Rex-Xue-Lin <39583370+Rex-Xue-Lin@users.noreply.github.com>
2018-05-30 23:56:28 +1000

    Add files via upload

---
+++ design.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 30 00:42:18 2018
+
+@author: Rex
+"""
+
+
+import numpy as np
+
+ 
+
+def design(THRESHOLD_MAX_INTERNAL_FUEL, THRESHOLD_MAX_OSCILLATION_INTERNAL_FUEL,
+alpha_b_array, alpha_p_array, max_internal_biofuel, 
+oscillation_internal_biofuel, final_external_biofuel) :
+    #alpha_b_array
+    
+    print(max_internal_biofuel)
+    
+    
+    # finding poor
+    
+    index = (np.argmax(final_external_biofuel))
+    print(index)
+    poor_alpha_b = (alpha_b_array[index//len(alpha_p_array)])
+    poor_alpha_p = (alpha_p_array[index%len(alpha_p_array)])
+    
+    
+    # condition
+    c = ((max_internal_biofuel < THRESHOLD_MAX_INTERNAL_FUEL) & (oscillation_internal_biofuel < THRESHOLD_MAX_OSCILLATION_INTERNAL_FUEL))
+    print(c)
+    
+    # set all false values to 0 
+    # change to negative infinity?
+    (final_external_biofuel[~c]) = 0
+    print(final_external_biofuel)
+    
+    
+    # finding best
+    index = (np.argmax(final_external_biofuel))
+    print(index)
+    best_alpha_b = (alpha_b_array[index//len(alpha_p_array)])
+    best_alpha_p = (alpha_p_array[index%len(alpha_p_array)])
+    
+ 
+    return(best_alpha_b, best_alpha_p, poor_alpha_b, poor_alpha_p)
